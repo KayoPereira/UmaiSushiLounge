10d0c5bd303343709b1770ad4e35bc9acf1fc528
KayoPereira <68305073+KayoPereira@users.noreply.github.com>
2020-11-05 22:48:12 -0300

    Adicionado o server

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const server = express()
 
-server.listen(8080, () => {
-    console.log('Servidor online em http://localhost:8080')
+server.get('/', (req, res) => {
+    res.sendFile(__dirname + "/views/index.html")
 })
+server.use(express.static(__dirname + '/views'));
+server.use(express.static(__dirname + '/public/'));
+
+server.listen(8080)
